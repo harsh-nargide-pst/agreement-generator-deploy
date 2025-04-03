306c5fea9b774d5189d2f573cfd1dcafbdfcdf20
Shruti Kalani <shruti.kalani@positsource.com>
2025-04-03 18:38:38 +0530

    Increased the spacing

--- backend/helpers/agreement_generator_helper.py
+++ backend/helpers/agreement_generator_helper.py
@@ -97,7 +97,7 @@ def get_styles(font_name: str, font_file: Optional[str]) -> Dict[str, ParagraphS
             parent=styles['Normal'],
             fontName=font_name if font_file else "Helvetica",
             fontSize=11,
-            spaceAfter=12,
+            spaceAfter=8,
             alignment=4,
             leading=18
         ),
